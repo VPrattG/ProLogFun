6b0221d818d3662602a62741c74252bde3eac986
VP-21212581 <l21212581@tectijuana.edu.mx>
2024-09-27 20:12:08 -0700

    Agregar comentarios en app_app y handler

--- CowboyServer/my_cowboy_app/apps/my_cowboy_app/src/my_cowboy_app_app.erl
+++ CowboyServer/my_cowboy_app/apps/my_cowboy_app/src/my_cowboy_app_app.erl
@@ -1,3 +1,12 @@
+%% --------------------------------------------------------------
+%% Módulo: my_cowboy_app_app
+%% Autor: Van Pratt González Ricardo Adolfo
+%% Description: Este módulo es donde se indica que módulo se llamara
+%% según la ruta.
+%%
+%%
+%% Fecha: 27 de septiembre de 2024
+%% --------------------------------------------------------------
 -module(my_cowboy_app_app).
 -behaviour(application).
 
@@ -9,14 +18,17 @@ start(_Type, _Args) ->
     %% Definir las rutas
     Dispatch = cowboy_router:compile([
         {'_', [
+            %% Por el momento, ambas rutas se manejan desde un mismo archivo, pero podrían usarse varias funciones
             {"/", my_cowboy_handler, []},
-	    {"/saludo", my_cowboy_handler, []}
+            {"/saludo", my_cowboy_handler, []}
         ]}
     ]),
     %% Iniciar Cowboy en el puerto 8080
-    {ok, _} = cowboy:start_clear(http_listener,
+    {ok, _} = cowboy:start_clear(
+        http_listener,
         [{port, 8080}],
-        #{env => #{dispatch => Dispatch}}),
+        #{env => #{dispatch => Dispatch}}
+    ),
     my_cowboy_app_sup:start_link().
 
 %% Detener la aplicación
--- CowboyServer/my_cowboy_app/apps/my_cowboy_app/src/my_cowboy_handler.erl
+++ CowboyServer/my_cowboy_app/apps/my_cowboy_app/src/my_cowboy_handler.erl
@@ -1,46 +1,65 @@
+%% --------------------------------------------------------------
+%% Módulo: my_cowboy_handler
+%% Autor: Van Pratt González Ricardo Adolfo
+%% Description: my_cowboy_handler contiene la información a imprimir
+%% al recibir ciertas peticiones en la aplicación web desarrollada.
+%% En particular, se tienen funciones para "/" y "/saludo"
+%%
+%% Fecha: 27 de septiembre de 2024
+%% --------------------------------------------------------------
+
 -module(my_cowboy_handler).
 -export([init/2]).
 
 init(Req0, State) ->
-    io:format("Request: ~p~n", [Req0]),  %% Imprimir el Request para depuración
+    %% Imprimir el Request para depuración
+    io:format("Request: ~p~n", [Req0]),
     Method = cowboy_req:method(Req0),
     Path = cowboy_req:path(Req0),
     {ok, handle_request(Method, Path, Req0), State}.
 
 handle_request(<<"GET">>, <<"/saludo">>, Req) ->
+    %% Si se recibe una petición de saludo, se ejecuta la función para saludar con todo y nombre
     handle_saludo(Req);
 handle_request(<<"GET">>, <<"/favicon.ico">>, Req) ->
     %% Manejar solicitudes de favicon.ico (común en navegadores)
     cowboy_req:reply(204, Req);
 handle_request(_, _, Req) ->
+    %% Por defecto, realizará un simple "hola mundo"
     handle_default(Req).
 
 %% Manejador para la ruta por defecto
 handle_default(Req) ->
-    cowboy_req:reply(200,
+    cowboy_req:reply(
+        200,
         #{<<"content-type">> => <<"text/plain">>},
         <<"Hola Mundo desde Cowboy!">>,
-        Req).
+        Req
+    ).
 
 %% Manejador para la ruta /saludo
 handle_saludo(Req) ->
     %% Extraer los parámetros de la consulta
     QS = cowboy_req:parse_qs(Req),
-    
+
     %% Buscar el valor del parámetro 'nombre'
     case proplists:get_value(<<"nombre">>, QS) of
         undefined ->
-            %% Si no se proporciona el nombre, devolver un error 400
-            cowboy_req:reply(400,
+            %% Si no se proporciona el nombre, se devuelve un error 400
+            cowboy_req:reply(
+                400,
                 #{<<"content-type">> => <<"text/plain">>},
                 <<"Error: parámetro 'nombre' no proporcionado">>,
-                Req);
+                Req
+            );
         Nombre ->
-            %% Construir el saludo personalizado
+            %% Crear el saludo con el nombre
             Saludo = <<"¡Hola, ", Nombre/binary, "!">>,
             %% Enviar la respuesta con el saludo
-            cowboy_req:reply(200,
+            cowboy_req:reply(
+                200,
                 #{<<"content-type">> => <<"text/plain">>},
                 Saludo,
-                Req)
+                Req
+            )
     end.
