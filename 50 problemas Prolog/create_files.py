4b0977105e2aa51ff1d4403def97053b5ae00370
VPrattG <147211017+VPrattG@users.noreply.github.com>
2024-10-19 12:37:14 -0700

    Delete 50 problemas Prolog/create_files.py

--- 50 problemas Prolog/create_files.py
+++
@@ -1,4 +0,0 @@
-for i in range(1, 51):
-    filename = f'programa{i:02}.pl'  # Format number with leading zero
-    with open(filename, 'w') as f:
-        f.write(f'# This is {filename}\n')  # Optional: add a comment inside the file
